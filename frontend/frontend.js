65614de1c263a8615612b94a11d81b8f02e0402a
Maximilian Bala <max_b98@hotmail.de>
2022-04-21 13:49:39 +0100

    fix apikey insert

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,2 +1,2 @@
-$('#pregame-embed').val(`${location.href}/gfx/pregame/index.html${window.apiKey !== null ? '?apikey' + window.apiKey : ''}`);
-$('#loading-embed').val(`${location.href}/gfx/loading/index.html${window.apiKey !== null ? '?apikey' + window.apiKey : ''}`);
+$('#pregame-embed').val(`${location.href}/gfx/pregame/index.html${window.apiKey !== null ? '?apikey=' + window.apiKey : ''}`);
+$('#loading-embed').val(`${location.href}/gfx/loading/index.html${window.apiKey !== null ? '?apikey=' + window.apiKey : ''}`);
