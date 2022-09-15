e4d16f1f526652bf2915130f60d4ba2df54c0745
Himyu <max_b98@hotmail.de>
2022-09-15 23:22:17 +0200

    update js for new structures

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,10 +1,13 @@
-$('#pregame-embed').val(
-  `${location.href}/gfx/pregame/index.html${
-    window.apiKey !== null ? '?apikey=' + window.apiKey : ''
-  }`
-)
-$('#loading-embed').val(
-  `${location.href}/gfx/loading/index.html${
-    window.apiKey !== null ? '?apikey=' + window.apiKey : ''
-  }`
-)
+async function initUI () {
+  const port =  await window.constants.getWebServerPort()
+  const location = `http://localhost:${port}/pages/op-module-valorant-pregame/gfx`
+
+  const apiKey =  await window.constants.getApiKey()
+
+  document.querySelector('#loading-embed').value = `${location}/loading/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
+  document.querySelector('#loading-gfx').value = `${location}/loading/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
+
+  document.querySelector('#pregame-embed').value = `${location}/pregame/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
+  document.querySelector('#pregame-gfx').src = `${location}/pregame/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
+}
+initUI()
