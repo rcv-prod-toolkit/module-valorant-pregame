6686dda48956cce275e0913edb26f46581d1cc01
Himyu <max_b98@hotmail.de>
2022-10-19 18:38:41 +0200

    update server link

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,13 +1,27 @@
-async function initUI () {
-  const port =  await window.constants.getWebServerPort()
-  const location = `http://localhost:${port}/pages/op-module-valorant-pregame/gfx`
+async function initUI() {
+  const server = await window.constants.getWebServerPort()
+  const location = `http://${server}/pages/op-module-valorant-pregame/gfx`
 
-  const apiKey =  await window.constants.getApiKey()
+  const apiKey = await window.constants.getApiKey()
 
-  document.querySelector('#loading-embed').value = `${location}/loading/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
-  document.querySelector('#loading-gfx').value = `${location}/loading/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
+  document.querySelector(
+    '#loading-embed'
+  ).value = `${location}/loading/index.html${
+    apiKey !== null ? '?apikey=' + apiKey : ''
+  }`
+  document.querySelector(
+    '#loading-gfx'
+  ).value = `${location}/loading/index.html${
+    apiKey !== null ? '?apikey=' + apiKey : ''
+  }`
 
-  document.querySelector('#pregame-embed').value = `${location}/pregame/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
-  document.querySelector('#pregame-gfx').src = `${location}/pregame/index.html${apiKey !== null ? '?apikey=' + apiKey: ''}`
+  document.querySelector(
+    '#pregame-embed'
+  ).value = `${location}/pregame/index.html${
+    apiKey !== null ? '?apikey=' + apiKey : ''
+  }`
+  document.querySelector('#pregame-gfx').src = `${location}/pregame/index.html${
+    apiKey !== null ? '?apikey=' + apiKey : ''
+  }`
 }
 initUI()
